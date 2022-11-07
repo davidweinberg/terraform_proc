6192e2bf13f2491a67ee7f6fba9db81b1a2484ba
David Weinberg <david.weinberg@anlayzere.com>
2022-11-07 08:44:56 -0500

    removed keys

--- dev/main.tf
+++ dev/main.tf
@@ -1,11 +1,6 @@
 provider "aws" {
-  access_key = "AKIAR5NSQ2ZULWMWVZUW"
-  secret_key = "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"
+  access_key = "***"
+  secret_key = "***"
   region     = var.aws_region
 }
-
-#module "ssm-patch-management" {
-#  source  = "jparnaudeau/ssm-patch-management/aws"
-##  version = "1.1.2"
-#}
 
