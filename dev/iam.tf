771e419d5b4ecbf460f6b4224587b9848c7d1773
David Weinberg <david.weinberg@anlayzere.com>
2022-11-07 09:43:29 -0500

    work window iam stuff added

--- dev/iam.tf
+++ dev/iam.tf
@@ -23,3 +23,34 @@ resource "aws_iam_instance_profile" "ssm_policy" {
   role = aws_iam_role.ssm_role.name
 }
 
+###############
+# Create Custom Role for patchManagement
+# and attach AmazonSSMMaintenanceWindowRole policy to the role
+###############
+resource "aws_iam_role" "ssm_maintenance_window" {
+  name = "role-${var.environment}-ssm-mw-role"
+  path = "/system/"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": ["ec2.amazonaws.com","ssm.amazonaws.com"]
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_role_policy_attachment" "role_attach_ssm_mw" {
+  role       = aws_iam_role.ssm_maintenance_window.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonSSMMaintenanceWindowRole"
+}
+
